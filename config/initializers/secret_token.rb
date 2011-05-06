dbf3648830c96d83de96bae60619e45affadd7b2
Edwin Vega <edwin.ponc@gmail.com>
2011-05-05 22:52:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demorails::Application.config.secret_token = 'a5533d25d98730be4c22757850121691665d0f872a7ab5799465f1f7aa69aee18d8d012ba85c828270f2706d996bb5e9554e684e12a41493d5e5765d0fba75cb'
